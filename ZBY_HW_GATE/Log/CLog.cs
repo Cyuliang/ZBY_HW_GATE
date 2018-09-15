bc5e3d63c3c70fac61388c8de96706f504c5aab1
lench <lench870888@hotmail.com>
2018-09-15 16:30:45 +0800

    Cyuliang@Log

---
+++ ZBY_HW_GATE/Log/CLog.cs
@@ -0,0 +1,17 @@
+﻿using log4net;
+
+namespace ZBY_HW_GATE
+{
+    class CLog
+    {
+        public ILog logError = LogManager.GetLogger("ErrorLog");
+        public ILog logDebug = LogManager.GetLogger("DebugLog");
+        public ILog logInfo = LogManager.GetLogger("InfoLog");
+        public ILog logWarn = LogManager.GetLogger("WarnLog");
+
+        public CLog()
+        {
+
+        }
+    }
+}
